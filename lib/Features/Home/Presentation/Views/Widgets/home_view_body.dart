29b108a509a033c7c18f4bb0124fb93e3ffa0e56
Shahd reda <shahdreda3@gmail.com>
2023-07-29 09:43:53 +0300

    Finishig book details view

--- lib/Features/Home/Presentation/Views/Widgets/home_view_body.dart
+++ lib/Features/Home/Presentation/Views/Widgets/home_view_body.dart
@@ -4,14 +4,14 @@ import '../../../../../Core/utils/styles.dart';
 import 'best_seller_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const CustomScrollView(
       slivers: [
-         SliverToBoxAdapter(
-            child: Column(
+         SliverToBoxAdapter(child: Column(
                crossAxisAlignment:CrossAxisAlignment.start,
                children: [
                  CustomAppBar(),
